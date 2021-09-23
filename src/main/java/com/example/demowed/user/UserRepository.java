22f94b7f12c6bf28fc89824504e73ad12c8c87c0
NguyenXuanTu <tutom16299@gmail.com>
2021-09-23 22:28:32 +0700

    commit

---
+++ src/main/java/com/example/demowed/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demowed.user;
+
+import com.example.demowed.user.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
+    public Long countById(Integer id);
+}
